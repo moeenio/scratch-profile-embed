6d6372f1ce79f21716f31de6ab5bb7cb9f178251
Locness <locness3@orange.fr>
2018-07-30 12:57:58 +0200

    Button edit

--- js/getcode.js
+++ js/getcode.js
@@ -1,11 +1,11 @@
-﻿var updateEmbedCode = function() {
+var updateEmbedCode = function() {
   var embedCode = document.getElementById("embed-code");
   var width = document.getElementById("width-input").value;
   var height = document.getElementById("height-input").value;
   var username = document.getElementById("username-input").value;
   var codeDemo = document.getElementById("code-demo");
-  embedCode.textContent = "<iframe width='" + width.toString() + "' height='" + height.toString() + "' src='https://locness3.github.io/scratch-profile-embed/#" + username + "'></iframe>";
-  codeDemo.innerHTML =  "<iframe width='" + width.toString() + "' height='" + height.toString() + "' src='https://locness3.github.io/scratch-profile-embed/#" + username + "'></iframe>";
+  embedCode.textContent = "<iframe width='" + width.toString() + "' height='" + height.toString() + "' src='https://thimbleprojects.org/locness/517367/#" + username + "'></iframe>";
+  codeDemo.innerHTML =  "<iframe width='" + width.toString() + "' height='" + height.toString() + "' src='https://thimbleprojects.org/locness/517367/#" + username + "'></iframe>";
   codeDemo.style.width = width + "px";
 }
 updateEmbedCode();
